52367f8274d93d27c077f7b0c789ee10b3679494
Marcus Minhorst <marcus.minhorst@gmail.com>
2025-10-11 19:47:03 -0400

    Remove hardcoded colours

--- src/common/widgets/netstartwindow.cpp
+++ src/common/widgets/netstartwindow.cpp
@@ -155,10 +155,6 @@ bool NetStartWindow::NetLoop(bool (*loopCallback)(void*), void* data)
 
 NetStartWindow::NetStartWindow(bool host) : Widget(nullptr, WidgetType::Window)
 {
-	SetWindowBackground(Colorf::fromRgba8(51, 51, 51));
-	SetWindowBorderColor(Colorf::fromRgba8(51, 51, 51));
-	SetWindowCaptionColor(Colorf::fromRgba8(33, 33, 33));
-	SetWindowCaptionTextColor(Colorf::fromRgba8(226, 223, 219));
 	SetWindowTitle(GAMENAME);
 
 	MessageLabel = new TextLabel(this);
